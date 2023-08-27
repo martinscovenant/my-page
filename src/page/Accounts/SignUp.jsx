d6d812c4d72c49d643d9dd4f871ac3ee8c7fa457
Covenant Martins <martinscovenant1@gmail.com>
2023-08-27 22:00:48 +0100

    my page

---
+++ src/page/Accounts/SignUp.jsx
@@ -0,0 +1,162 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './SignUp.css';
+
+
+const SignUp = () => {
+  // ============= Initial State Start here =============
+  const [clientName, setClientName] = useState ("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // const [checked, setChecked] = useState(true);
+  // ============= Initial State End here ===============
+  // ============= Error Msg Start here =================
+  const [errClientName, setErrClientName] = useState("");
+  const [errEmail, setErrEmail] = useState("");
+  const [errPassword, setErrPassword] = useState("");
+
+  // ============= Error Msg End here ===================
+  const [successMsg, setSuccessMsg] = useState("");
+  // ============= Event Handler Start here =============
+  const handleName = (e) => {
+    setClientName(e.target.value);
+    setErrClientName("");
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setErrEmail("");
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setErrPassword("");
+  };
+  // ============= Event Handler End here ===============
+  // ================= Email Validation start here =============
+  const EmailValidation = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
+  };
+  // ================= Email Validation End here ===============
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    // if (checked) {
+      if (!clientName) {
+        setErrClientName("Enter your name");
+      }
+      if (!email) {
+        setErrEmail("Enter your email");
+      } else {
+        if (!EmailValidation(email)) {
+          setErrEmail("Enter a Valid email");
+        }
+      }
+      if (!password) {
+        setErrPassword("Create a password");
+      } else {
+        if (password.length < 6) {
+          setErrPassword("Passwords must be at least 6 characters");
+        }
+      }
+
+      // ============== Getting the value ==============
+      if (
+        clientName &&
+        email &&
+        EmailValidation(email) &&
+        password &&
+        password.length >= 6 
+      ) {
+        setSuccessMsg(
+          `Hello dear ${clientName}, Thank u for joining us. We received your Sign up request. We are processing to validate your access. Till then stay connected and additional assistance will be sent to you by your mail at ${email}`
+        );
+        setClientName("");
+        setEmail("");
+        setPassword ("");
+      }
+    // }
+  };
+  return (
+      <div className="App">
+        {successMsg ? (
+          <div className="w-[500px]">
+            <p className="success">
+              {successMsg}
+            </p>
+            <Link to="/signIn">
+              <button
+                className="bts"
+              >
+                Sign in
+              </button>
+            </Link>
+          </div>
+        ) : (
+          <form className="done">
+            <div className="done2">
+              <h3>
+                Sign up
+              </h3>
+                  <input
+                    onChange={handleName}
+                    value={clientName}
+                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
+                    type="text"
+                    placeholder="enter your full name"
+                  />
+                  {errClientName && (
+                    <p className="name">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errClientName}
+                    </p>
+                  )}
+                  <input
+                    onChange={handleEmail}
+                    value={email}
+                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
+                    type="email"
+                    placeholder="martins@gmail.com"
+                  />
+                  {errEmail && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errEmail}
+                    </p>
+                  )}
+                  <input
+                    onChange={handlePassword}
+                    value={password}
+                    className="w-full h-8 placeholder:text-sm placeholder:tracking-wide px-4 text-base font-medium placeholder:font-normal rounded-md border-[1px] border-gray-400 outline-none"
+                    type="password"
+                    placeholder="Create password"
+                  />
+                  {errPassword && (
+                    <p className="text-sm text-red-500 font-titleFont font-semibold px-4">
+                      <span className="font-bold italic mr-1">!</span>
+                      {errPassword}
+                    </p>
+                  )}
+                  
+                <button
+                  onClick={handleSignUp}
+                >
+                  Create Account
+                </button>
+                <p className="text-sm text-center font-titleFont font-medium">
+                  Don't have an Account?{" "}
+                  <Link to="/signin">
+                    <span className="hover:text-blue-600 duration-300">
+                      Sign in
+                    </span>
+                  </Link>
+                </p>
+              </div>
+            {/* </div> */}
+          </form>
+        )}
+      </div>
+  );
+};
+
+export default SignUp;
