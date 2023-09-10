975d0715b401ea86c3d2e48ed9040d2ca3143083
Covenant Martins <martinscovenant1@gmail.com>
2023-09-10 02:51:17 +0100

    satuday's commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 // import { SignUp } from './page/Accounts/SignUp';
-import { SignIn } from './page/Accounts/SignIn';
+// import { SignIn } from './page/Accounts/SignIn';
 // import { ViewAllReport } from './components/viewAllReport/ViewAllReport';
 // import { Modal } from './components/modal/Modal';
-// import {UpdateReport } from './components/update/UpdateReport';
+import {UpdateReport } from './components/update/UpdateReport';
 import { Route, Routes } from 'react-router-dom';
  
 
@@ -13,10 +13,10 @@ function App() {
  <Routes>
     
      {/* <Route path='/' element={<SignUp/>}/> */}
-   <Route path='/SignIn' element={<SignIn/>}/>   
+   {/* <Route path='/SignIn' element={<SignIn/>}/>    */}
    {/* <Route path='/' element={<ViewAllReport/>}/> */}
     {/* <Route path='/' element={<Modal />}/>  */}
-    {/* <Route path='/' element={<UpdateReport />}/> */}
+    <Route path='/UpdateReport' element={<UpdateReport />}/>
    </Routes>
    );
   
