b3f4bde958f542c1275c10b67f3bd6bd8e37eafa
Covenant Martins <martinscovenant1@gmail.com>
2023-09-14 18:42:27 +0100

    Thursday commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import { Header } from './components/header/Header'
 // import { SignUp } from './page/Accounts/SignUp';
 // import { SignIn } from './page/Accounts/SignIn';
 // import { ViewAllReport } from './components/viewAllReport/ViewAllReport';
 // import {UpdateReport } from './components/update/UpdateReport';
-import { Footer } from "./components/footer/Footer";
-import { FooterBottom }from "./components/footer/FooterBottom";
+// import { Footer } from "./components/footer/Footer";
+// import { FooterBottom }from "./components/footer/FooterBottom";
 import { Route, Routes } from 'react-router-dom';
  
 
@@ -12,12 +13,13 @@ function App() {
 
    return(
  <Routes>
+  <Route path='/' element={< Header/>}/>
      {/* <Route path='/' element={<SignUp/>}/>
    <Route path='/SignIn' element={<SignIn/>}/>   
    <Route path='/' element={<ViewAllReport/>}/> */}
     {/* <Route path='/UpdateReport' element={<UpdateReport />}/> */}
-      <Route path='/' element={<Footer/>}/>
-      <Route path='/' element={<FooterBottom/>}/>
+      {/* <Route path='/' element={<Footer/>}/>
+      <Route path='/' element={<FooterBottom/>}/> */}
    </Routes>
    );
   
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,12 @@
 
 import React, { useState } from 'react'
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import { NavLink , useLocation} from 'react-router-dom';
+import './Header.css';
 
 
-export const header = () => {
+export const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
   const location = useLocation();
   const toggleMenu = () => {
@@ -13,63 +14,38 @@ export const header = () => {
   };
 
   return <>
-  <header className='navbar-middle sticky-top p-2 p-md-2 p-lg-2'>
-    <div className="row align-items-center m-0">
-      <div className="col-md-2 d-flex justify-content-center">
-
-      <button className="navbar-toggler d-md-none " type="button" onClick={toggleMenu}>
+  <header className='header'>
+    <div className="row ">
+      <div className="row2">
+      <button className="btn" type="button" onClick={toggleMenu}>
           <span className="navbar-toggler-icon">{showMenu ? <AiOutlineClose /> : <AiOutlineMenu /> }
           </span>
         </button>
-
+             
       <div className="col-md-10 row col-lg-10">
-        <div className="col-md-3 m-auto">
-        <div className="input-group d-none d-md-flex">
-          <input type="text" className="form-control" placeholder="Find products ..." aria-label="Find products ..." aria-describedby="basic-addon2" />
-          <button className="input-group-text" id="basic-addon2">search</button>
+        <div className="search-container">
+
+          <input type="text" 
+          className="form-control" 
+          placeholder="Search ..." 
+          aria-label="Search ..." 
+          aria-describedby="basic-addon2" 
+          />
+
+          <button 
+          className="input" 
+          id="basic-addon2">search
+          </button>
         </div>
+
+        <div className="nav-list">
+        <div className='nav-item'>
+          <div className='nav-links'><NavLink to="/" className={location.pathname === '/' ? 'active' : 'not-active'} onClick={toggleMenu}>Home</NavLink></div>
+          <div className='nav-links'><NavLink to="/shop" className={location.pathname === '/shop' ? 'active' : 'not-active'} onClick={toggleMenu}>Report</NavLink></div>
+          <div className='nav-links'><NavLink to="/blog" className={location.pathname === '/blog' ? 'active' : 'not-active'} onClick={toggleMenu}>View</NavLink></div>
+          <div className='nav-links'><NavLink to="/about" className={location.pathname === '/about' ? 'active' : 'not-active'} onClick={toggleMenu}>About</NavLink></div>
+          <div className='nav-links'><NavLink to="/contact" className={location.pathname === '/contact' ? 'active' : 'not-active'} onClick={toggleMenu}>Contact</NavLink></div>
         </div>
-        <div className="col-md-6 m-auto">
-        <div className='menu-links mt-2 d-none d-md-flex d-lg-flex'>
-          <div className='ms-auto gap-3'><NavLink to="/" className={location.pathname === '/' ? 'active' : 'not-active'} onClick={toggleMenu}>HOME</NavLink></div>
-          <div className='ms-auto gap-3'><NavLink to="/shop" className={location.pathname === '/shop' ? 'active' : 'not-active'} onClick={toggleMenu}>Report</NavLink></div>
-          <div className='ms-auto gap-3'><NavLink to="/blog" className={location.pathname === '/blog' ? 'active' : 'not-active'} onClick={toggleMenu}>View</NavLink></div>
-          <div className='ms-auto gap-3'><NavLink to="/about" className={location.pathname === '/about' ? 'active' : 'not-active'} onClick={toggleMenu}>About</NavLink></div>
-          <div className='ms-auto gap-3'><NavLink to="/contact" className={location.pathname === '/contact' ? 'active' : 'not-active'} onClick={toggleMenu}>CONTACT</NavLink></div>
-        </div>
-        </div>
-        <div className="col-md-3">
-        <div className="row d-flex justify-content-center">
-            <div className="col-12 col-md-2 d-none d-md-flex d-lg-flex m-auto">
-              <div className={location.pathname === '/' ? 'active' : 'not-active'}>
-                <Link onClick={toggleMenu}
-                  to=""
-                  className="d-flex align-items-center color-nav me-3"
-                >
-                  <CgProductHunt className='me-1 fs-2' />
-                </Link>
-              </div>
-              <div className={location.pathname === 'login' ? 'active' : 'not-active'}>
-                <Link onClick={toggleMenu}
-                  to="/login"
-                  className="d-flex align-items-center color-nav me-3"
-                >
-                  <VscAccount className='me-1 fs-2' />
-                </Link>
-              </div>
-              <div className={location.pathname === 'cart' ? 'active' : 'not-active'}>
-                <Link onClick={toggleMenu}
-                  to="/cart"
-                  className="d-flex align-items-center color-nav me-3 cart-span-one"
-                >
-                  <CgShoppingCart className='me-1 fs-2' />
-                  <div >
-                  {/* <p><b><span>{totalProducts}</span></b></p> */}
-                  </div>
-                </Link>
-              </div>
-            </div>
-          </div>
         </div>
         
       </div>
@@ -86,17 +62,7 @@ export const header = () => {
             <div className='mb-2'><NavLink className={location.pathname === '/' ? 'active' : 'not-active'} to="/" onClick={toggleMenu}>ABOUT</NavLink></div>
             <div className='mb-2'><NavLink className={location.pathname === '/' ? 'active' : 'not-active'} to="/" onClick={toggleMenu}>CONTACT</NavLink></div>
           </div>
-        
-        <div className="menu-bar__actions">
-        <div className='mb-2'>
-          <Link to="/SignIn" className={location.pathname === '/SignIn' ? 'active' : 'not-active'} onClick={toggleMenu}>
-            <img src={user} alt="user" className='d-none' />
-            <span>Account</span>
-          </Link>
-        </div>
       </div>
-      </div>
-
       )}
     </div>
   </div>
