ff825dc1abc560708b04d155187d49c5f08afad9
Covenant Martins <martinscovenant1@gmail.com>
2023-11-02 11:14:40 +0100

    Thursday commit

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,11 +1,17 @@
 
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import './Home.css'
 
 export const Home = () => {
+
   const [apiResponse, setApiResponse] = useState ("");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
   React.useEffect(() => {
   }, []);
   useEffect(() => {
@@ -24,6 +30,28 @@ export const Home = () => {
   }, []);
 
   return (
+    <div>
+      <header className="navbar">
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/ViewSpecificReport">View</a></li>
+          <li><a href="/UpdateReport">Update</a></li>
+          <li><a href="/UpdateUserName">User</a></li>
+      <div className="dropdown">
+       <button className="dropdown-button" onClick={toggleDropdown}>
+          Accounts
+        </button>
+        {isOpen && (
+          <div className="dropdown-content">
+            {/* <a href="/Signup">Signup</a> */}
+            <a href="/Signin">Login</a>
+          </div>
+        )}
+      </div>
+      </ul>
+      </nav>
+    </header>
     <section className="banner">
       <div className="">
         {/* <p>{apiResponse}</p> */}
@@ -35,15 +63,12 @@ export const Home = () => {
           <div className="carousel-inner">
             <div className="back-details">
               <p className="mb-3"></p>
-
-              {/* <Link to="/" className="center  btn-primary" id="button-link">
-                Learn more
-              </Link> */}
             </div>
           </div>
         </div>
       </div>
     </section>
+    </div>
   );
 };
 
